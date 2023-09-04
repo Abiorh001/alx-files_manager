3600a0fa1b1703e6ab782fb5341cc88af6eb1654
maiyo008 <tonymaiyo008@gmail.com>
2023-09-04 22:00:06 +0300

    ESLint linter hot fix

--- utils/redis.js
+++ utils/redis.js
@@ -2,36 +2,34 @@ const redis = require('redis');
 const { promisify } = require('util');
 
 class RedisClient {
-    constructor() {
-        this.client = redis.createClient();
-        this.client.on('error', (err) => {
-            console.error(`Redis client error: ${err}`);
-        });
-        
-    }
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (err) => {
+      console.error(`Redis client error: ${err}`);
+    });
+  }
 
-    isAlive() {
-        return this.client.connected;
-    }
+  isAlive() {
+    return this.client.connected;
+  }
 
-    async get(key) {
-        const getAsync = promisify(this.client.get).bind(this.client);
-        return getAsync(key);
-    }
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    return getAsync(key);
+  }
 
-    async set(key, value, duration) {
-        const setAsync = promisify(this.client.set).bind(this.client);
-        if (duration) {
-            return setAsync(key, value, 'EX', duration);
-        } else {
-            return setAsync(key, value);
-        }
+  async set(key, value, duration) {
+    const setAsync = promisify(this.client.set).bind(this.client);
+    if (duration) {
+      return setAsync(key, value, 'EX', duration);
     }
+    return setAsync(key, value);
+  }
 
-    async del(key) {
-        const delAsync = promisify(this.client.del).bind(this.client);
-        return delAsync(key);
-    }
+  async del(key) {
+    const delAsync = promisify(this.client.del).bind(this.client);
+    return delAsync(key);
+  }
 }
 
 const redisClient = new RedisClient();
